509ff4990574af878cbb0053b4ecb7b88df349f4
mammothbane <itsamammoth@gmail.com>
2014-01-19 17:44:56 -0500

    added new task item to project menu, enabled clicking on projects

--- QueuerAutobots/src/main/java/com/autobots/queuer/activities/ProjectActivity.java
+++ QueuerAutobots/src/main/java/com/autobots/queuer/activities/ProjectActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
+import com.autobots.queuer.R;
+
 /**
  * Created by mammothbane on 1/17/14.
  */
@@ -11,12 +13,16 @@ public class ProjectActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_project, menu);
+        //return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+
 }
 
